c5963cc3393dbf411d204e6f50dd8a446d43e374
boysHe <2413091005@qq.com>
2017-04-22 15:32:33 +0800

    1232

---
+++ app/src/main/java/app/app/MainActivity.java
@@ -0,0 +1,20 @@
+package app.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+
+import rx.Observable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("hsTest","这是测试的呀！");
+
+    }
+}
